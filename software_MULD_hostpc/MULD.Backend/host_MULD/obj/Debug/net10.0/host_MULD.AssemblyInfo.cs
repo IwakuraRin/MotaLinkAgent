bdd51a3595a3ace76d6464b14268bc4a6e067a80
IwakuraRin <IwakuraTorei@outlook.com>
2026-03-30 02:57:41 +0800

    修复了一些已知的bug

--- software_MULD_hostpc/MULD.Backend/host_MULD/obj/Debug/net10.0/host_MULD.AssemblyInfo.cs
+++ software_MULD_hostpc/MULD.Backend/host_MULD/obj/Debug/net10.0/host_MULD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("host_MULD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138763bd1fd4742e18f9d2f095b7ce8ff8759a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27658697d049ff56c3b39bcca4347b1625d7f442")]
 [assembly: System.Reflection.AssemblyProductAttribute("host_MULD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("host_MULD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
